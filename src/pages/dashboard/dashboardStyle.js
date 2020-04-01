9bf98e1bf30df5671800541e9814e3f11fc06691
Vinicius <viniciusvieiradearaujo@hotmail.com>
2020-03-31 22:08:32 -0300

    shimmer addition

--- src/pages/dashboard/dashboardStyle.js
+++ src/pages/dashboard/dashboardStyle.js
@@ -11,13 +11,13 @@ export default StyleSheet.create({
     header:{
         height: 55,
         flexDirection: 'row',
-        borderBottomWidth: .3,
+        //borderBottomWidth: .3,
         borderBottomColor: '#43485C',
-        
+        shadowColor: 'black',
+        elevation: 2
     },
     hambBox:{
         justifyContent: 'center',
-        //backgroundColor: 'red',
         marginLeft: 15,
     },
     titleBox:{
@@ -30,5 +30,19 @@ export default StyleSheet.create({
         color:'snow',
         fontSize: 20,
         fontWeight: 'bold'
+    },
+    body:{
+        flex: 1,
+        alignItems:'center'
+    },
+    item:{
+        width: 300,
+        height: 150,
+        borderRadius: 8,
+        backgroundColor: '#43485C',
+        margin: 20,
+    },
+    shimmer:{
+        margin: 10,
     }
 });
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,22 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { SimpleLineIcons } from '@expo/vector-icons'
 
 import style from './dashboardStyle'
+import Shimmer from '../../Shimmer'
 
 const DashBoard = () => {
-    return(
-        <View style={style.container}>
-            <View style={style.header}>
-                <TouchableOpacity style={style.hambBox}>
-                    <SimpleLineIcons name="menu" size={28} color="white"/>
-                </TouchableOpacity>
-                <View style={style.titleBox}>
-                    <Text style={style.title}>Noticias</Text>
+    const [loading, setLoading] = useState(true);
+    
+    if(loading){
+        return(
+            <View style={style.container}>
+                <View style={style.header}>
+                    <TouchableOpacity style={style.hambBox}>
+                        <SimpleLineIcons name="menu" size={28} color="white"/>
+                    </TouchableOpacity>
+                    <View style={[style.titleBox, {marginLeft: -40,}]}>
+                       <Shimmer width={130} height={18}/>
+                    </View>
+                </View>
+                <View style={style.body}>
+                    <View style={style.item}>
+                        <View style={style.shimmer}>
+                            <Shimmer width={200} height={20}/>
+                        </View>
+                        <View style={style.shimmer}>
+                            <Shimmer width={200} height={20}/>
+                        </View>
+                    </View>
                 </View>
             </View>
-        </View>
-    );
+        );
+    }else{
+        return(
+            <View style={style.container}>
+                <View style={style.header}>
+                    <TouchableOpacity style={style.hambBox}>
+                        <SimpleLineIcons name="menu" size={28} color="white"/>
+                    </TouchableOpacity>
+                    <View style={style.titleBox}>
+                        <Text style={style.title}>Noticias</Text>
+                    </View>
+                </View>
+                <View style={style.body}>
+                    <View style={style.item}>
+                        <View style={style.shimmer}>
+                            <Shimmer width={200} height={20}/>
+                        </View>
+                        <View style={style.shimmer}>
+                            <Shimmer width={200} height={20}/>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        );
+    }
 }
 
 
