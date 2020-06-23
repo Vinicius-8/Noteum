4f381ae56946b78121ec73949fc2fa92ca1576d1
Vinicius <viniciusvieiradearaujo@hotmail.com>
2020-06-23 01:06:40 -0300

    Icon fixies

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 //http://noteum-backend-production.herokuapp.com
+//http://192.168.0.101:8000
 const api = axios.create({
-    baseURL: 'http://192.168.0.101:8000'
+    baseURL: 'http://noteum-backend-production.herokuapp.com:80'
 });
 
 export default api;
